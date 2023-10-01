fd58c01e054fe2d39255fc75745477aa4aa787ac
josefellipe <josefserqueira@gmail.com>
2023-10-01 14:39:49 -0300

    optimize the views

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.tasks, name='tasks'),
+    path('addTask/', views.addTask, name='addTask'),
+    path('deleteTask/', views.deleteTask, name='deleteTask'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,15 +1,11 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect
 from .forms import TaskForm
 from .models import Task
 
 def tasks(request):
-    tasks = Task.objects.all()
-    print(tasks)
+    tasks = Task.objects.all().order_by('-created_at')
 
     form = TaskForm()
-    if request.method == 'POST':
-        form = TaskForm(request.POST)
-        print(form.data['description'])
 
     context = {
         'form': form,
@@ -17,3 +13,22 @@ def tasks(request):
     }
 
     return render(request, 'tasks.html', context)
+
+
+def addTask(request):
+
+    description = request.POST['description']
+    Task.objects.create(
+        description=description
+    )
+
+    return redirect('/tasks')
+
+
+def deleteTask(request):
+
+    if request.method == 'POST':
+        id = request.POST
+        print(id)
+
+    return redirect('/tasks')
